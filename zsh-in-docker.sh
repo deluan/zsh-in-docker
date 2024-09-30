cb315e12a047ba60fc32164bfc071bd858b60e39
TanShaochang <30321432+petertheprocess@users.noreply.github.com>
2024-09-30 20:55:59 +0200

    allow user pass their own TERM env, allow right color showing for zsh-autosuggestion. (#30)

--- zsh-in-docker.sh
+++ zsh-in-docker.sh
@@ -121,7 +121,7 @@ zshrc_template() {
 export LANG='en_US.UTF-8'
 export LANGUAGE='en_US:en'
 export LC_ALL='en_US.UTF-8'
-export TERM=xterm
+[ -z "$TERM" ] && export TERM=xterm
 
 ##### Zsh/Oh-my-Zsh Configuration
 export ZSH="$_HOME/.oh-my-zsh"
